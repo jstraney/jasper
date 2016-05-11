79cf815d220f8b13aa261a2a2a72fac8218d86c7
Jeff <Jeff@MN5-JSTRANEY-MB.local>
2016-05-10 21:54:07 -0400

    added functionality to timer util

--- js/util.js
+++ js/util.js
@@ -1,11 +1,25 @@
 (function (jas) {
-  function timer() {
+  function timer(interval, isRandom) {
     var then;
     var done;
-    var interval;
+    var originalInterval = interval || null;
+    var interval = interval || null;
+    var timeSet = interval ? true: false;
+    var isRandom = isRandom || false;
     
     function start () {
+      originalInterval = originalInterval || 0;
+      interval = interval || 0;
+      timeSet = true;
       then = Date.now();
+    }
+    
+    function contractInterval(amount) {
+      interval -= amount;
+    }
+    
+    function expandInterval(amount) {
+      interval += amount;
     }
     
     function stop () {
@@ -13,7 +27,13 @@
     }
     
     function setTimer (duration) {
-      interval = duration;
+      if (isRandom) {
+        interval = (Math.random() * duration);
+      }
+      else {
+        interval = duration;  
+      }
+      
     }
     
     function checkTime (itsTime, notTime) {
@@ -21,24 +41,42 @@
         return;
       }
       var now = Date.now();
+      
       if (now - then >= interval) {
         if (typeof(itsTime) == "function") {
           itsTime();
         }
+        then = now;
+        if (isRandom) {
+          setTimer(originalInterval);  
+        }
+        return true;
       }
       else {
+        //console.log(getTime());
         if (typeof(notTime) == "function") {
           notTime();
         }
+        
+        return false;
       }
-      then = now;
+      
+    }
+    
+    function getStart() {
+      return then;
+    }
+    
+    function getTime () {
+      return Date.now() - then;
     }
     
     return {
       start: start,
       stop: stop,
       setTimer: setTimer,
-      checkTime: checkTime  
+      checkTime: checkTime,
+      getTime: getTime
     }
   }
   
